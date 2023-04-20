8b35a9ce9f63868088f2fa774ea6f0077232717d
Juan Bindez <juanbindez780@gmail.com>
2023-04-20 13:50:29 -0300

    Update mgs.py

--- mgs.py
+++ mgs.py
@@ -1,13 +1,27 @@
 import random
-import numpy as np
-import time
+
+while len(numeros_sorteados) < 6:
+    numero = random.randint(1, 60)
+    if numero not in numeros_sorteados:
+        numeros_sorteados.append(numero)
+
+        if len(numeros_sorteados) == 1:
+            card_1 = numero
+        elif len(numeros_sorteados) == 2:
+            card_2 = numero
+        elif len(numeros_sorteados) == 3:
+            card_3 = numero
+        elif len(numeros_sorteados) == 4:
+            card_4 = numero
+        elif len(numeros_sorteados) == 5:
+            card_5 = numero
+        else:
+            card_6 = numero
 
 
-def loop():
-    while 1 < 2:
-        x = np.random.randint(1,60, (6,6))
-        print(x)
-        time.sleep(1)
-
-
-loop()
+print(card_1)
+print(card_2)
+print(card_3)
+print(card_4)
+print(card_5)
+print(card_6)
